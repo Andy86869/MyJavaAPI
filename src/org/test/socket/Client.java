7f239e531d8667e8c3f6c756604d570d4a99a784
Andy <robbincen@163.com>
2016-09-27 15:42:41 +0800

    客户端code

---
+++ src/org/test/socket/Client.java
@@ -0,0 +1,41 @@
+package org.test.socket;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+	public static void main(String[] args) {
+		try {
+			// 1创建客户端Socekt
+			Socket socket = new Socket("127.0.0.1", 8888);
+			// 构建IO
+			InputStream is = socket.getInputStream();
+			OutputStream os = socket.getOutputStream();
+
+			// 向服务器发送一条消息
+			PrintWriter pw = new PrintWriter(os);
+			pw.write("用户名:admin;密码:123");
+			pw.flush();
+			socket.shutdownOutput();
+			// 读取服务器返回的消息
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			String info = null;
+			while ((info = br.readLine()) != null) {
+				System.out.println("我是客户端,服务端对我说:" + info);
+			}
+			// 关闭资源
+			br.close();
+			is.close();
+			pw.close();
+			os.close();
+			socket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
