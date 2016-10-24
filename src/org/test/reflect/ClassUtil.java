0c98b4ce719d5065f95d01a5ca2cb0326527b1bf
Andy <robbincen@163.com>
2016-10-24 20:49:31 +0800

    add static method

--- src/org/test/reflect/ClassUtil.java
+++ src/org/test/reflect/ClassUtil.java
@@ -1,17 +1,21 @@
 package org.test.reflect;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 /**
  * @author Andy Class API -
  */
 public class ClassUtil {
-	
+
 	public static void main(String[] args) {
 		String s = "hello";
 		int a = 123;
-		printClassMessage(s);
+//		printClassMessage(s);
+		printConMessage(s);
 	}
+
 	/**
 	 * 打印类的信息,包括类的函数,类的成员变量
 	 * 
@@ -38,10 +42,50 @@ public class ClassUtil {
 			// 获取参数类型-->得到的是参数列表的类型的类类型
 			Class[] paramTypes = ms[i].getParameterTypes();
 			for (Class class1 : paramTypes) {
-				System.out.print(class1.getName()+",");
+				System.out.print(class1.getName() + ",");
 			}
 			System.out.println(")");
 		}
 
+		/**
+		 * 成员变量也是对象 java.lang.reflect.Field Field 封装了关于成员变量的操作
+		 * getFields()获取的是所有的public的成员变量 getDeclaredFields()获取的是该类自己申明的成员变量的信息
+		 */
+		// Field[] fs = c1.getFields();
+		Field[] fs = c1.getDeclaredFields();
+		for (Field field : fs) {
+			// 得到成员变量类型的类类型
+			Class fieldType = field.getType();
+			String typeName = fieldType.getName();
+			// 得到成员变量的名称
+			String fieldName = field.getName();
+			System.out.println(typeName + "" + fieldName);
+		}
+
+	}
+
+	/**
+	 * 打印对的构造函数的信息
+	 * 
+	 * @param obj
+	 */
+	public static void printConMessage(Object obj) {
+		Class c = obj.getClass();
+		/**
+		 * 构造函数也是对象
+		 * java.lang.Constructor 中封装了构造函数的信息
+		 * getConstructors()获取所有的public的构造函数
+		 * getDeclaredConstructors()得到所有的构造函数
+		 */
+		Constructor[] cs = c.getConstructors();
+		for (Constructor constructor : cs) {
+			System.out.print(constructor.getName()+"(");
+			//获取构造函数的参数列表-> 得到参数列表的类类型
+			Class[] paramTypes = constructor.getParameterTypes();
+			for (Class class1 : paramTypes) {
+				System.out.print(class1.getName()+",");
+			}
+			System.out.println(")");
+		}
 	}
 }
